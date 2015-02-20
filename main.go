7024605aa472c8c9e79ded6d65567e15e1f3467c
Pirmin Fix <ookami.gnu@gmail.com>
2015-02-20 23:45:34 +0100

    fully working compare dir

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+var help = `Usage: %v FIRST_DIR SECOND_DIR
+`
+
+func mapDir(name string) (map[[16]byte]string, error) {
+	fullPath, err := filepath.Abs(name)
+	if err != nil {
+		return nil, err
+	}
+	dirlist, err := ioutil.ReadDir(fullPath)
+	if err != nil {
+		return nil, err
+	}
+	m := make(map[[16]byte]string, len(dirlist))
+	for _, item := range dirlist {
+		file, err := os.Open(filepath.Join(fullPath, item.Name()))
+		if err != nil {
+			return m, err
+		}
+		data, err := ioutil.ReadAll(file)
+		m[md5.Sum(data)] = item.Name()
+	}
+	return m, nil
+
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf(help, os.Args[0])
+	}
+	local, err := mapDir(os.Args[1])
+	remote, err := mapDir(os.Args[2])
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	for k := range local {
+		if _, ok := remote[k]; ok {
+			fmt.Printf("%v\t%v\n", local[k], remote[k])
+		}
+	}
+}
